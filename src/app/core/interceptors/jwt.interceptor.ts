13999b56776b642f9d79817d33c0c778f1a399b9
Jere-c <71843734+Jere-c@users.noreply.github.com>
2024-11-21 21:48:18 -0300

    login con auth andando

--- src/app/core/interceptors/jwt.interceptor.ts
+++ src/app/core/interceptors/jwt.interceptor.ts
@@ -7,7 +7,7 @@ import { LoginService } from '../services/login.service';
 
 export const jwtInterceptor: HttpInterceptorFn = (req, next) => {
     // Omitir el interceptor si la solicitud es para la ruta de inicio de sesión
-    if (req.url.includes('/tabs/tab2')) {
+    if (req.url.includes('/login')) {
         return next(req); // Deja pasar la request sin modificar
     }
 
@@ -19,14 +19,14 @@ export const jwtInterceptor: HttpInterceptorFn = (req, next) => {
         // Clonar el request para insertar el token
         const request = req.clone({
             setHeaders: {
-                authorization: `Bearer ${token}`, // Nota: la cabecera es sensible a mayúsculas
+                Authorization: `Bearer ${token}`, // Nota: la cabecera es sensible a mayúsculas
             },
         });
 
         // Manejar errores con un pipe de observables
         return next(request).pipe(
             catchError((e) => {
-                if (e instanceof HttpErrorResponse ? e.status === 401:false) {
+                if (e instanceof HttpErrorResponse && e.status === 401) {
                     auth.logout();
                     return throwError(() => new Error('Unauthorized'));
                 } else {
@@ -35,7 +35,6 @@ export const jwtInterceptor: HttpInterceptorFn = (req, next) => {
             })
         );
     } else {
-        console.log(token)
         // Si no existe token, desloguear al usuario
         auth.logout();
         return throwError(() => new Error('No token provided'));
