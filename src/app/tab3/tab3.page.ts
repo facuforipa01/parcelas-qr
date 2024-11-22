88f2cacb5673e47eba2856ec375353b3de8389b9
facuforipa <facufolatelli2001@gmail.com>
2024-11-21 21:49:40 -0300

    intento de q muestre el qr iconos lindos

--- src/app/tab3/tab3.page.ts
+++ src/app/tab3/tab3.page.ts
@@ -11,17 +11,28 @@ code:any
   constructor() {}
 
   async scanQRCode() {
-    try {
-      const result = await BarcodeScanner.startScan();
-      if (result.hasContent) {
-        console.log('Scanned content:', result.content);
-        alert('Scanned QR Code: ' + result.content);
-      } else {
-        alert('No QR code found');
+    const status = await BarcodeScanner.checkPermission({ force: true });
+  
+    if (status.granted) {
+      await BarcodeScanner.hideBackground(); // Oculta el fondo de la aplicación para que la cámara sea visible.
+      
+      try {
+        const result = await BarcodeScanner.startScan();
+        if (result.hasContent) {
+          console.log('Scanned content:', result.content);
+          alert('Scanned QR Code: ' + result.content);
+        } else {
+          alert('No QR code found');
+        }
+      } catch (err) {
+        console.log('Error scanning QR code:', err);
+        alert('Error scanning QR');
+      } finally {
+        await BarcodeScanner.showBackground(); // Restaura el fondo después del escaneo.
       }
-    } catch (err) {
-      console.log('Error scanning QR code:', err);
-      alert('No QR');
+    } else {
+      alert('Camera permission denied');
     }
   }
+  
 }
